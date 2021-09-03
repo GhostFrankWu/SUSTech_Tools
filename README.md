45d59c4f271aaaec3a9e3e3c1744866bee2df4f7
Frank <56958034+GhostFrankWu@users.noreply.github.com>
2021-09-03 15:52:03 +0800

    更新课程id获取逻辑

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # SUSTech_Tools
 
 ## 南科大tis选课脚本[使用说明](https://github.com/GhostFrankWu/SUSTech_Tools/blob/main/tis%E6%8A%A2%E8%AF%BE%E8%84%9A%E6%9C%AC/ReadMe.md)  
-最后一次检测可用是2021-7-27，检测结果是 **不可用**  
+最后一次检测可用是2021-7-27，检测结果是 **可用**  
 - 如果您本地有python3.7+环境并希望手动运行/修改代码[访问源代码](https://github.com/GhostFrankWu/SUSTech_Tools/tree/main/tis%E6%8A%A2%E8%AF%BE%E8%84%9A%E6%9C%AC)  
 - 如果您本地没有python环境，您可以[使用windows打包版](https://github.com/GhostFrankWu/SUSTech_Tools/releases/tag/2021_1_19)  
 
 
 | 最新日志 | 目前是否可用 | 时间 |
 -- | -- | -- 
-| 检测可用 | x | 2021-9-2 |
 | 检测可用 | √ | 2021-7-27 |
 | 检测可用 | √ | 2021-6-13 |
 | 稳定更新 | √ | 2021-1-11 |
--- tis抢课脚本/main.py
+++ tis抢课脚本/main.py
@@ -1,118 +1,142 @@
 import requests
-import time
-from random import *
+from json import loads
+from getpass import getpass
 from re import findall
-from json import load
 from os import path
 import _thread
 
-def caslogin(userName,passWord):
-    loginUrl="https://cas.sustech.edu.cn/cas/login?service=https%3A%2F%2Ftis.sustech.edu.cn%2Fcas" #removed Login
-    req=requests.get(loginUrl)
-    if(req.status_code == 200):
-        print("成功连接到CAS...")
-    else:
-        print("请检查您的网络连接状态")
-        return "",""
+
+def caslogin(token, userName, passWord):
+    print("[\x1b[0;36m!\x1b[0m] " + "测试CAS链接...")
+    try:
+        loginUrl = "https://cas.sustech.edu.cn/cas/login?service=https%3A%2F%2Ftis.sustech.edu.cn%2Fcas"  # removed Login
+        req = token.get(loginUrl)
+        assert (req.status_code == 200)
+        print("[\x1b[0;32m+\x1b[0m] " + "成功连接到CAS...")
+    except:
+        print("[\x1b[0;31mx\x1b[0m] " + "不能访问CAS, 请检查您的网络连接状态")
+        return "", ""
+    print("[\x1b[0;36m!\x1b[0m] " + "登录中...")
     data = {
         'username': userName,
         'password': passWord,
         'execution': str(req.text).split('''name="execution" value="''')[1].split('"')[0],
         '_eventId': 'submit',
     }
-    req=requests.post(loginUrl, data=data, allow_redirects=False)
-    if("Location" in req.headers.keys()):
-        print("登录成功...")
+    req = token.post(loginUrl, data=data, allow_redirects=False)
+    if "Location" in req.headers.keys():
+        print("[\x1b[0;32m+\x1b[0m] " + "登录成功")
     else:
-        print("用户名或密码错误，请检查")
-        return "",""
-    req=requests.get(req.headers["Location"], allow_redirects=False)
-    route=findall('route=(.+?);', req.headers["Set-Cookie"])[0]
-    JSESSIONID=findall('JSESSIONID=(.+?);', req.headers["Set-Cookie"])[0]
-    return route,JSESSIONID
-    
-def submit(route,JSESSIONID,id):
+        print("[\x1b[0;31mx\x1b[0m] " + "用户名或密码错误，请检查")
+        return "", ""
+    req = token.get(req.headers["Location"], allow_redirects=False)
+    route = findall('route=(.+?);', req.headers["Set-Cookie"])[0]
+    JSESSIONID = findall('JSESSIONID=(.+?);', req.headers["Set-Cookie"])[0]
+    return route, JSESSIONID
+
+
+def getinfo(route, JSESSIONID):
     headers = {
-        "cookie": 'route={}; JSESSIONID={};'.format(route,JSESSIONID),
+        "cookie": 'route={}; JSESSIONID={};'.format(route, JSESSIONID),
         "user-agent": "Mozilla/5.0",
         "x-requested-with": "XMLHttpRequest"
     }
     data = {
-         "p_pylx": 1,
-        "p_sfgldjr": 0,#是否管理端进入
+        "p_xn": "2021-2022",
+        "p_xq": 1,
+        "p_xnxq": "2021-20221",
+        "p_chaxunpylx": 3,
+        "mxpylx": 3,
+        "p_sfhltsxx": 0,
+        "pageNum": 1,
+        "pageSize": 2000}
+    req = requests.post('https://tis.sustech.edu.cn/Xsxktz/queryRwxxcxList', data=data, headers=headers)
+    return req.text
+
+
+def submit(route, JSESSIONID, id):
+    headers = {
+        "cookie": 'route={}; JSESSIONID={};'.format(route, JSESSIONID),
+        "user-agent": "Mozilla/5.0",
+        "x-requested-with": "XMLHttpRequest"
+    }
+    data = {
+        "p_pylx": 1,
+        "p_sfgldjr": 0,  # 是否管理端进入
         "p_sfredis": 0,
         "p_sfsyxkgwc": 0,
-        "p_xktjz": "rwtjzyx",#提交至，可选任务，rwtjzgwc提交至购物车，rwtjzyx提交至已选 gwctjzyx购物车提交至已选
+        "p_xktjz": "rwtjzyx",  # 提交至，可选任务，rwtjzgwc提交至购物车，rwtjzyx提交至已选 gwctjzyx购物车提交至已选
         "p_xn": "2020-2021",
         "p_xq": 2,
         "p_xnxq": "2020-20212",
         "p_dqxn": "2020-2021",
         "p_dqxq": 2,
         "p_dqxnxq": "2020-20211",
-        "p_xkfsdm": "bxxk",#补选选课
-        "p_id":id, #课程id
-        "p_sfhlctkc":0,#是否忽略冲突课程
-        "p_sfhllrlkc":0,#是否忽略零容量课程
-        "p_sfxsgwckb":1,#固定
-        }
-    req = requests.post('https://tis.sustech.edu.cn/Xsxk/addGouwuche',data=data,headers=headers)
-    t=req.text 
-    print(t)
+        "p_xkfsdm": "bxxk",  # 补选选课
+        "p_id": id,  # 课程id
+        "p_sfhlctkc": 0,  # 是否忽略冲突课程
+        "p_sfhllrlkc": 0,  # 是否忽略零容量课程
+        "p_sfxsgwckb": 1,  # 固定
+    }
+    req = requests.post('https://tis.sustech.edu.cn/Xsxk/addGouwuche', data=data, headers=headers)
+    print(req.text)
 
-if __name__ =='__main__':
-    classData = {}
-    cachePath = "userCache.json"
-    if path.exists(cachePath) and path.isfile(cachePath):
-        print("读取课程信息...")
-        with open(cachePath, "r", encoding="utf8") as f:
-            classData = load(f)["classData"][0]
-            f.close()
-        print("课程信息读取完毕...")
-    else:
-       print("课程信息丢失，请检查")
-       exit(1)
+
+if __name__ == '__main__':
+    token = requests.session()
+    # 获取要抢的课程
     cachePath = "Class.txt"
     classList = []
     if path.exists(cachePath) and path.isfile(cachePath):
-        print("读取规划课表...")
+        print("[\x1b[0;36m!\x1b[0m] " + "读取规划课表...")
         with open(cachePath, "r", encoding="utf8") as f:
-            classList = f.readlines();
-        print("规划课表读取完毕...")
+            classList = f.readlines()
+        print("[\x1b[0;32m+\x1b[0m] " + "规划课表读取完毕")
     else:
-        print("没有找到规划课表，请手动输入课程信息，输入-1结束录入")
-        while(True):
-            s=input()
-            if(s=="-1"):
+        print("[\x1b[0;33m-\x1b[0m] " + "没有找到规划课表，请手动输入课程信息，输入-1结束录入")
+        while True:
+            s = input()
+            if s == "-1":
                 break
             else:
                 classList.append(s)
-        s=input("是否保存录入的信息（Y/N）？")
-        if(s=="Y" or s=="y"):
+        s = input("是否保存录入的信息（Y/N）？")
+        if s == "Y" or s == "y":
             with open(cachePath, "w", encoding="utf8") as f:
-                f.write("===本文件是待选课程的列表，一行输入一个课程名字==请勿删除本行===\n");
+                f.write("===本文件是待选课程的列表，一行输入一个课程名字==请勿删除本行===\n")
                 for i in classList:
-                    f.write(i+"\n")
+                    f.write(i + "\n")
                 f.close()
+    # CAS登录
+    route, JSESSIONID = "", ""
+    while route == "" or JSESSIONID == "":
+        userName = input("请输入您的学号：")
+        passWord = getpass("请输入CAS密码（密码不显示，输入完按回车即可）：")
+        route, JSESSIONID = caslogin(token, userName, passWord)
+        if route == "" or JSESSIONID == "":
+            print("请重试...")
+    # 获取课程信息
+    classData = {}
+    print("[\x1b[0;36m!\x1b[0m] " + "从服务器下载课程信息...")
+    rawClassData = loads(getinfo(route, JSESSIONID))
+    for i in rawClassData['rwList']['list']:
+        classData[i['rwmc']]=i['id']
+    print("[\x1b[0;32m+\x1b[0m] " + "课程信息读取完毕")
+    print("[\x1b[0;34m{}\x1b[0m]".format("="*20))
+    # 准备要抢的课程
     postList = []
     for name in classList:
-        name=name.strip()
-        if (name in classData.keys()) :
+        name = name.strip()
+        if name in classData.keys():
             postList.append(classData[name])
             print(name)
-    print("成功读入以上信息")
-    route,JSESSIONID="",""
-    while(route=="" or JSESSIONID==""):
-        userName=input("请输入您的学号：")
-        passWord=input("请输入CAS 密码：")
-        route,JSESSIONID=caslogin(userName,passWord)
-        if(route=="" or JSESSIONID==""):
-            print("请重试...")
-    while(True):
-        input("按任意键选课")
-        for id in postList :
+    print("[\x1b[0;34m{}\x1b[0m]".format("="*25))
+    print("[\x1b[0;32m+\x1b[0m] " + "成功读入以上信息")
+    # 抢课主逻辑
+    while True:
+        input("[\x1b[0;32m+\x1b[0m] " + "按任意键抢一次，多按同时抢多次")
+        for id in postList:
             try:
-                _thread.start_new_thread(submit,(route,JSESSIONID,id) )
+                _thread.start_new_thread(submit, (route, JSESSIONID, id))
             except:
-                print ("线程异常")
-
-            
+                print("线程异常")
