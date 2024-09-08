13660e6c082e007dfc9e0c9e6bbe409bd5d31f68
GhostFrankWu <FrankssWu@outlook.com>
2024-09-08 19:45:24 +0800

    lint main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 main.py 南科大TIS喵课助手
 
 @CreateDate 2021-1-9
-@UpdateDate 2022-6-20
+@UpdateDate 2024-9-8
 """
 
 import _thread
@@ -21,45 +21,47 @@ import warnings
 from urllib3.exceptions import InsecureRequestWarning
 
 
-def warn(message, category, filename, lineno, file=None, line=None):
+def warn(message, category, filename, lineno, _file=None, line=None):
     if category is not InsecureRequestWarning:
         sys.stderr.write(warnings.formatwarning(message, category, filename, lineno, line))
 
 
 warnings.showwarning = warn
-
+SUCCESS = "[\x1b[0;32m+\x1b[0m] "
+ERROR = "[\x1b[0;31mx\x1b[0m] "
+INFO = "[\x1b[0;36m!\x1b[0m] "
+UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.0.0 Safari/537.36"
 head = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.0.0 Safari/537.36",
+    "user-agent": UA,
     "x-requested-with": "XMLHttpRequest"
 }
 
 
-def cas_login(user_name, pwd):
+def cas_login(sid, pwd):
     """ 用于和南科大CAS认证交互，拿到tis的有效cookie
     输入用于CAS登录的用户名密码，输出tis需要的全部cookie内容(返回头Set-Cookie段的route和jsessionid)
     我的requests的session不吃CAS重定向给到的cookie，不知道是代码哪里的问题，所以就手动拿了 """
-    print("[\x1b[0;36m!\x1b[0m] " + "测试CAS链接...")
+    print(INFO + "测试CAS链接...")
     try:  # Login 服务的CAS链接有时候会变
         login_url = "https://cas.sustech.edu.cn/cas/login?service=https%3A%2F%2Ftis.sustech.edu.cn%2Fcas"
-        # login_url = "https://cas.sustech.edu.cn/cas/clientredirect?client_name=Wework&service=https%3A%2F%2Ftis.sustech.edu.cn%2Fcas"
         req = requests.get(login_url, headers=head, verify=False)
         assert (req.status_code == 200)
-        print("[\x1b[0;32m+\x1b[0m] " + "成功连接到CAS...")
-    except:
-        print("[\x1b[0;31mx\x1b[0m] " + "不能访问CAS, 请检查您的网络连接状态")
+        print(SUCCESS + "成功连接到CAS...")
+    except Exception as ex:
+        print(ERROR + f"不能访问CAS, 请检查您的网络连接状态 ({ex})")
         return "", ""
-    print("[\x1b[0;36m!\x1b[0m] " + "登录中...")
+    print(INFO + "登录中...")
     data = {  # execution大概是CAS中前端session id之类的东西
-        'username': user_name,
+        'username': sid,
         'password': pwd,
         'execution': str(req.text).split('''name="execution" value="''')[1].split('"')[0],
         '_eventId': 'submit',
     }
     req = requests.post(login_url, data=data, allow_redirects=False, headers=head, verify=False)
     if "Location" in req.headers.keys():
-        print("[\x1b[0;32m+\x1b[0m] " + "登录成功")
+        print(SUCCESS + "登录成功")
     else:
-        print("[\x1b[0;31mx\x1b[0m] " + "用户名或密码错误，请检查")
+        print(ERROR + "用户名或密码错误，请检查")
         return "", ""
     req = requests.get(req.headers["Location"], allow_redirects=False, headers=head, verify=False)
     route_ = findall('route=(.+?);', req.headers["Set-Cookie"])[0]
@@ -71,7 +73,8 @@ def getinfo(semester_data):
     """ 用于向tis请求当前学期的课程ID，得到的ID将用于选课的请求
     输入当前学期的日期信息，返回的json包括了课程名和内部的ID """
     course_list = []
-    course_types = {'bxxk': "通识必修选课", 'xxxk': "通识选修选课", "kzyxk": '培养方案内课程', "zynknjxk": '非培养方案内课程', "jhnxk": '计划内选课新生'}
+    course_types = {'bxxk': "通识必修选课", 'xxxk': "通识选修选课", "kzyxk": '培养方案内课程',
+                    "zynknjxk": '非培养方案内课程', "jhnxk": '计划内选课新生'}
     for course_type in course_types.keys():
         data = {
             "p_xn": semester_data['p_xn'],  # 当前学年
@@ -96,14 +99,13 @@ def getinfo(semester_data):
                 if name in class_data.keys():
                     course_list.append([class_data[name], course_type, name])
 
-    print("[\x1b[0;32m+\x1b[0m] " + "课程信息读取完毕")
+    print(SUCCESS + "课程信息读取完毕")
     print("[\x1b[0;34m{}\x1b[0m]".format("=" * 25))
     for course in course_list:
         print(course_types[course[1]] + " : " + course[2], end="")
         print("   ID 为: " + course[0])
     print("[\x1b[0;34m{}\x1b[0m]".format("=" * 25))
-    print("[\x1b[0;32m+\x1b[0m] " + "成功读入以上信息")
-    print()
+    print(SUCCESS + "成功读入以上信息\n")
     return course_list
 
 
@@ -137,10 +139,10 @@ def load_course():
     cache_path = "Class.txt"
     classes = []
     if path.exists(cache_path) and path.isfile(cache_path):
-        print("[\x1b[0;36m!\x1b[0m] " + "读取规划课表...")
+        print(INFO + "读取规划课表...")
         with open(cache_path, "r", encoding="utf8") as f:
             classes = f.readlines()
-        print("[\x1b[0;32m+\x1b[0m] " + "规划课表读取完毕")
+        print(SUCCESS + "规划课表读取完毕")
     else:
         print("[\x1b[0;33m-\x1b[0m] " + "没有找到规划课表，请手动输入课程信息，输入-1结束录入")
         while True:
@@ -149,7 +151,7 @@ def load_course():
                 break
             else:
                 classes.append(s)
-        s = input("[\x1b[0;36m!\x1b[0m] " + "是否保存录入的信息（y/N）？")
+        s = input(INFO + "是否保存录入的信息（y/N）？")
         if s == "Y" or s == "y":
             with open(cache_path, "w", encoding="utf8") as f:
                 f.write("===本文件是待喵课程的列表，一行输入一个课程名字==请勿删除本行===\n")
@@ -165,31 +167,32 @@ if __name__ == '__main__':
     # 下面是CAS登录
     route, JSESSIONID = "", ""
     while route == "" or JSESSIONID == "":
-        userName = input("请输入您的学号：")  # getpass在PyCharm里不能正常工作，请改为input或写死
-        passWord = getpass("请输入CAS密码（密码不显示，输入完按回车即可）：")
-        route, JSESSIONID = cas_login(userName, passWord)
+        user_name = input("请输入您的学号：")  # getpass在PyCharm里不能正常工作，请改为input或写死
+        pass_word = getpass("请输入CAS密码（密码不显示，输入完按回车即可）：")
+        route, JSESSIONID = cas_login(user_name, pass_word)
         if route == "" or JSESSIONID == "":
             print("[\x1b[0;33m-\x1b[0m] " + "请重试...")
     head['cookie'] = f'route={route}; JSESSIONID={JSESSIONID};'
     # 下面先获取当前的学期
-    print("[\x1b[0;36m!\x1b[0m] " + "从服务器获取当前喵课时间...")
-    semester_info = loads(   # 这里要加mxpylx才能获取到选课所在最新学期
-        requests.post('https://tis.sustech.edu.cn/Xsxk/queryXkdqXnxq', data={"mxpylx": 1}, headers=head, verify=False).text)
-    print("[\x1b[0;32m+\x1b[0m] " + f"当前学期是{semester_info['p_xn']}学年第{semester_info['p_xq']}学期，为"
-                                    f"{['', '秋季', '春季', '小'][int(semester_info['p_xq'])]}学期")
+    print(INFO + "从服务器获取当前喵课时间...")
+    semester_info = loads(
+        requests.post('https://tis.sustech.edu.cn/Xsxk/queryXkdqXnxq',
+                      data={"mxpylx": 1}, headers=head, verify=False).text)  # 这里要加mxpylx才能获取到选课所在最新学期
+    print(SUCCESS + f"当前学期是{semester_info['p_xn']}学年第{semester_info['p_xq']}学期，为"
+                    f"{['', '秋季', '春季', '小'][int(semester_info['p_xq'])]}学期")
     # 下面获取课程信息
-    print("[\x1b[0;36m!\x1b[0m] " + "从服务器下载课程信息，请稍等...")
-    postList = getinfo(semester_info)
+    print(INFO + "从服务器下载课程信息，请稍等...")
+    post_list = getinfo(semester_info)
     # 喵课主逻辑
     while True:
-        print("[\x1b[0;32m+\x1b[0m] " + "按一下回车喵三次，多按同时喵多次")
+        print(SUCCESS + "按一下回车喵三次，多按同时喵多次")
         input()
-        for c_id in postList:
+        for c_id in post_list:
             try:
                 for _ in range(3):
                     _thread.start_new_thread(submit, (semester_info, c_id))
-            except:
-                print("线程异常")
+            except Exception as e:
+                print(f"[{e}] 线程异常")
 
 """
 # timing is everything!
